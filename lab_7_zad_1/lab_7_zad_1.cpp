d6d6fadb61a834d761e05a9cf2edad71b1b3397c
vladdaaa <144156817+vladdaaa@users.noreply.github.com>
2023-12-10 00:25:20 +0700

    Добавьте файлы проекта.

---
+++ lab_7_zad_1/lab_7_zad_1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+    char c;
+    cout << "Введите C:";
+    cin >> c;
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        cout << "lat" << " ";
+    else if (c >= '1' && c <= '9')
+        cout << "digit";
+    else
+        cout << "unknown";
+    return 0;
+}
